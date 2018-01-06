1be7d5c7720c947887169a4d6b45d7cc8e768b98
gkennedy <graeme.kennedy@ae.gatech.edu>
2018-01-06 09:35:26 -0500

    renamed files from .c to .cpp

--- TMR_Common.mk
+++ TMR_Common.mk
@@ -17,8 +17,8 @@ TMR_EXTERN_LIBS = ${BLOSSOM_LIB} ${TACS_LD_FLAGS} ${PAROPT_LD_FLAGS} ${OPENCASCA
 TMR_LD_FLAGS = ${TMR_LD_CMD} ${TMR_EXTERN_LIBS}
 
 # This is the one rule that is used to compile all the source
-%.o: %.c
+%.o: %.cpp
 	${CXX} ${TMR_CC_FLAGS} -c $< -o $*.o
 	@echo
-	@echo "        --- Compiled $*.c successfully ---"
+	@echo "        --- Compiled $*.cpp successfully ---"
 	@echo
