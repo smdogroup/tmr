ed5d9a31ff900c9666ba279b85408fde684c351d
gkennedy <graeme.kennedy@ae.gatech.edu>
2018-03-09 07:51:37 -0500

    added license statements and modified documentation to reflect recent changes

--- src/TMR_STLTools.h
+++ src/TMR_STLTools.h
@@ -1,3 +1,23 @@
+/*
+  This file is part of the package TMR for adaptive mesh refinement.
+
+  Copyright (C) 2015 Georgia Tech Research Corporation.
+  Additional copyright (C) 2015 Graeme Kennedy.
+  All rights reserved.
+
+  TMR is licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this software except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+  
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
 #ifndef TMR_STL_TOOLS_H
 #define TMR_STL_TOOLS_H
 
