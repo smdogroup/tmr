64759b6fe49ca20ab107606c60ba1e4cb22b17e5
yfu97 <aaronfu@gatech.edu>
2021-05-26 11:06:31 -0400

    update from pace

--- examples/topology_optimization/qn-correction/eigenvalue/core_data.py
+++ examples/topology_optimization/qn-correction/eigenvalue/core_data.py
@@ -1,10 +1,11 @@
 import os
 import argparse
+import re
 from shutil import copy
 
 p = argparse.ArgumentParser()
-p.add_argument('result-folder', type=str)
-p.add_argument('paropt-num', type=int)
+p.add_argument('result_folder', type=str)
+p.add_argument('paropt_num', type=int)
 args = p.parse_args()
 
 # Get all folders with name e-number-optimizer
@@ -27,20 +28,35 @@ for d in dirs:
 for d in dirs:
     e, num, omz = d.split('-')
 
-    copy(os.path.join(args.result_folder, d, 'output_refine0.pkl'),
-            os.path.join(out_folder, d))
+    try:
+        copy(os.path.join(args.result_folder, d, 'output_refine0.pkl'),
+                os.path.join(out_folder, d))
+    except:
+        print("cannot copy {:s}, file might not exist.".format(os.path.join(args.result_folder, d, 'output_refine0.pkl')))
 
     if omz == 'paropt' or omz == 'paroptqn':
-        copy(os.path.join(args.result_folder, d, 'tr_output_file0.dat'),
-             os.path.join(out_folder, d))
-        copy(os.path.join(args.result_folder, d, 'output{:d}.f5'.format(args.paropt_num)),
-             os.path.join(out_folder, d))
+        try:
+            copy(os.path.join(args.result_folder, d, 'tr_output_file0.dat'),
+                 os.path.join(out_folder, d))
+        except:
+            pass
+        try:
+            copy(os.path.join(args.result_folder, d, 'output{:d}.f5'.format(args.paropt_num)),
+                 os.path.join(out_folder, d))
+        except:
+            pass
 
     else:
-        copy(os.path.join(args.result_folder, d, omz+'_output_file0.dat'),
-             os.path.join(out_folder, d))
-        copy(os.path.join(args.result_folder, d, 'output_refine0.f5'),
-                os.path.join(out_folder, d))
+        try:
+            copy(os.path.join(args.result_folder, d, omz+'_output_file0.dat'),
+                 os.path.join(out_folder, d))
+        except:
+            pass
+        try:
+            copy(os.path.join(args.result_folder, d, 'output_refine0.f5'),
+                    os.path.join(out_folder, d))
+        except:
+            pass
 
 
 
