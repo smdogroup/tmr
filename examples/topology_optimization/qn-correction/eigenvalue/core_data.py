69a894ef9a46b52508644ca13adb45019cb492b2
aaronyicongfu <fuyicong1996@gmail.com>
2022-02-21 15:58:42 -0500

    update collector

--- examples/topology_optimization/qn-correction/eigenvalue/core_data.py
+++ examples/topology_optimization/qn-correction/eigenvalue/core_data.py
@@ -2,13 +2,14 @@ import os
 import argparse
 import re
 from shutil import copy
+from glob import glob
 
 p = argparse.ArgumentParser()
 p.add_argument('result_folder', type=str)
-p.add_argument('paropt_num', type=int)
 p.add_argument('start', type=int)
 p.add_argument('end', type=int)
 p.add_argument('problem', type=str)
+p.add_argument('--single_optimizer', action='store_true')
 args = p.parse_args()
 
 # Get all folders with name number-optimizer
@@ -29,58 +30,66 @@ for d in dirs:
 
 # Copy over output files
 for d in dirs:
+    print('populating {:s}'.format(os.path.join(out_folder, d)))
+
     num, omz = d.split('-')
 
+    # Copy over pickles
     try:
-        copy(os.path.join(args.result_folder, d, 'output_refine0.pkl'),
-                os.path.join(out_folder, d))
+        for pkl in glob(os.path.join(args.result_folder, d, 'output_refine*.pkl')):
+            copy(pkl, os.path.join(out_folder, d))
     except:
-        print("cannot copy {:s}, file might not exist.".format(os.path.join(args.result_folder, d, 'output_refine0.pkl')))
+        print('no output_refine*.pkl in {:s}'.format(os.path.join(args.result_folder, d)))
 
+    # Copy over failing eigenvalue f5 file
     try:
         copy(os.path.join(args.result_folder, d, 'fail.f5'),
              os.path.join(out_folder, d))
     except:
         pass
 
+    # Copy over outputs
+    if omz == 'paropt' or omz == 'paroptqn':
+        prefix = 'tr_output_file'
+    elif omz == 'mma':
+        prefix = 'mma_output_file'
+    else:
+        prefix = omz+'_output_file'
+
+    try:
+        for dat in glob(os.path.join(args.result_folder, d, prefix+'*.dat')):
+            copy(dat, os.path.join(out_folder, d))
+    except:
+        print('no {:s}*.dat in {:s}'.format(prefix, os.path.join(args.result_folder, d)))
+
+    # Copy over f5
     if omz == 'paropt' or omz == 'paroptqn':
         try:
-            copy(os.path.join(args.result_folder, d, 'tr_output_file0.dat'),
-                 os.path.join(out_folder, d))
-        except:
-            pass
-        try:
-            copy(os.path.join(args.result_folder, d, 'output{:d}.f5'.format(args.paropt_num)),
-                 os.path.join(out_folder, d))
-        except:
-            pass
+            outputf5 = glob(os.path.join(args.result_folder, d, 'output*.f5'))
+            outputf5 = [os.path.basename(f5) for f5 in outputf5]  # Get basenames
+            outputf5.sort(key=lambda x: int(x.split('.')[0][6:]))  # Sort f5 by iteration number
+            copy(os.path.join(args.result_folder, d, outputf5[-1]), os.path.join(out_folder, d))
 
-    elif omz == 'mma':
-        try:
-            copy(os.path.join(args.result_folder, d, 'mma_output_file0.dat'),
-                 os.path.join(out_folder, d))
         except:
-            pass
+            print('failing to copy over f5 file from {:s}'.format(os.path.join(args.result_folder, d)))
 
     else:
         try:
-            copy(os.path.join(args.result_folder, d, omz+'_output_file0.dat'),
-                 os.path.join(out_folder, d))
+            for f5 in glob(os.path.join(args.result_folder, d, 'output_refine*.f5')):
+                copy(f5, os.path.join(out_folder, d))
         except:
-            pass
-        try:
-            copy(os.path.join(args.result_folder, d, 'output_refine0.f5'),
-                    os.path.join(out_folder, d))
-        except:
-            pass
+            print('failing to copy over f5 file from {:s}'.format(os.path.join(args.result_folder, d)))
 
-# Copy over stdouts
-for d in dirs:
-    num, omz = d.split('-')
+# for d in dirs:
+#    num, omz = d.split('-')
 
-    # Compute the number of stdout
+    # Copy over stdouts
+
     offset = {'paropt':1, 'paroptqn':2, 'snopt':3, 'ipopt':4, 'mma':5}
-    stdout_num = (int(num) - args.start)*len(offset) + offset[omz]
+    if args.single_optimizer:
+        stdout_num = int(num) - args.start + 1
+    else:
+        stdout_num = (int(num) - args.start)*len(offset) + offset[omz]
     stdout_name = '{:s}-n{:d}-{:d}.out-{:d}'.format(args.problem,
         args.start, args.end, stdout_num)
 
